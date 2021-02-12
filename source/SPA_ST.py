78706349f362fc45af551c7fc5ef7cd6452b7b3f
SaraBolouriB <boloorisara@gmail.com>
2021-02-12 17:41:57 +0330

    updated

--- source/SPA_ST.py
+++ source/SPA_ST.py
@@ -1,96 +1,133 @@
-def spa_st(Ss, Pp, Ll, students_pref, lecturers_pref, offers, projects_capacity, lecturers_capacity):
+from methods import *
 
-    #Variables --------------------------------------------------------------------------------------
-    students_pref_copy = list(students_pref)
-    lecturers_pref_copy = list(lecturers_pref)
-    project_lecturer = lecturer_offers(offers=offers)
+def spa_st(Ss, Pp, Ll, students_pref, lecturers_pref, offers, projects_capacity, lecturers_capacity):
+    '''
+        students_pref  --> dictionary
+        lecturers_pref --> dictionary
+        offers, projects_capacity, lecturers_capacity --> List
+    '''
+
+    #Variables -------------------------------------------------------------------------------------------
+    students_pref_copy = students_pref.copy()               #Copy of student preference list
+    lecturers_pref_copy = lecturers_pref.copy()             #Copy of lecturer preference list
+    project_lecturer = lecturer_proposed(offers=offers)     #Show which professor proposed each project
 
     number_of_stu = len(Ss)                                 #Number of student
     number_of_lect = len(Ll)                                #Number of lecturer
     number_of_proj = len(Pp)                                #Number of project
+                
+    Gp = [[] for row in range(number_of_proj)]    
+    Gl = [[] for row in range(number_of_lect)]                     
+    graph = [[] for row in range(number_of_stu)]            #Primary Graph
+    
+    Ljk = generate_Ljk( students_pref=students_pref, 
+                        lecturers_pref=lecturers_pref,
+                        offers=offers, 
+                        number_of_lect=number_of_lect)
 
-    Gs = initialization(number_of_stu)                  
-    Gp = initialization(number_of_proj)     
-    Gl = initialization(number_of_lect) 
-    LP_to_s = lp_initialization(number_of_lect, offers)
-    Ljk = 
-
-    graph = [[-1] for row in range(lenS)]       #Primary Graph
-    ## -----------------------------------------------------------------------------------------------
     lecturerC, projectC = [0 for i in range(number_of_lect)], [0 for i in range(number_of_proj)
     project_replete, lecturer_replete = [], []
+    ## ----------------------------------------------------------------------------------------------------
 
-    for student in range(number_of_stu):
-        if not Gs[student] and not studens_pref_copy[student]:
-            head_project = studens_pref_copy[student].pop(0)
+    while True:
+        while True:
+            while bool(students_pref_copy):                             #Whether all student assigened or not
+                student_info = students_pref_copy.popitem()             #Remove last student of list. return -> (key,value)
+                student = student_info[0]                               #Key   -> student index 
+                student_projectList = student_info[1]                   #Value -> preferences list of student
 
-            if type(head_project) == tuple:     #Is it tie?
-                lenL = len(head_project)
-            else:
-                lenL = 1
-            
-            for project in range(lenL):
-                lecturer = project_lecturer[project]
-                graph[student] = project
-                #Update Lists -------------------------------------------------------------------------
-                Gs[student] = project
-                Gp[project] = student
-                Gl[lecturer] = student
-                ##-------------------------------------------------------------------------------------
-                if project_capacity[project] <= projectC[project]:
-                    project_replete.append(project)
-                    alpha = calculate_alpha(project_capacity, Gp, offers[lecturer])
-                    check_domained = 
-                    pass
+                if student_projectList:
+                    head_project = to_list(student_projectList.pop(0))  #Thefirst preference, it may be just one project or tuple(tie) 
+                    
+                    for project in head_project:
+                        lecturer = project_lecturer[project]            #Find lecturer who proposed the project
+                        graph[student].append(project)
+                        #Update Variables -------------------------------------------------------------------
+                        Gp[project].append(student)
+                        Gl[lecturer].append(student)
+                        lecturerC[lecturer] += 1
+                        projectC[project] += 1
+                        ##-----------------------------------------------------------------------------------
+                        if projects_capacity[project] <= projectC[project]:
+                            project_replete.append(project)             #Add project to replete list
+                            alpha = calculate_alpha(projects_capacity=projects_capacity, Gp=Gp, projects=offers[lecturer])
+                            dominated_students = dominated_Lkj(Ljk=Ljk, lecturer=lecturer, project=project, Gp=Gp)
+                            for ds in dominated_students:
+                                if project in graph[ds]:
+                                    graph[ds].remove(project)
+                                    Gp[project].remove(ds)
+                                    Gl[lecturer].remove(ds)
+                                    lecturerC[lecturer] -= 1
+                                    projectC[project] -= 1
 
-                if lecturer_capacity[lecturer] <= lecturerC[lecturer]:
-                    lecturer_replete.append(lecturer)
-                    alpha = calculate_alpha(project_capacity, Gp, offers[lecturer])
-                    dominated_students = dominated( alpha=alpha,
-                                                    assign_students=list(Gl[lecturer]), 
-                                                    lecturer_capacity=lecturers_capacity[lecturer], 
-                                                    students=lecturers_pref[lecturer])
-                                                    
+                        if lecturer_capacity[lecturer] <= lecturerC[lecturer]:
+                            lecturer_replete.append(lecturer)
+                            alpha = calculate_alpha(projects_capacity=projects_capacity, Gp=Gp, projects=offers[lecturer])
+                            dominated_students = dominated_Lk( alpha=alpha,
+                                                                lecturer_capacity=lecturers_capacity[lecturer],
+                                                                lecturer_pref=lecturers_pref[lecturer],
+                                                                Gl=Gl,
+                                                                lecturer=lecturer)
+                            for ds in dominated_students:
+                                intersection_projects = intersection(A=students_pref[ds], B=offers[lecturer])
+                                for p in intersection_projects:
+                                    if p in graph[ds]:
+                                        graph[ds].remove(p)
+                                        Gp[project].remove(ds)
+                                        Gl[lecturer].remove(ds)
+                                        lecturerC[lecturer] -= 1
+                                        projectC[project] -= 1
                 
+            Gr = reduce_graph(graph=graph)              #Reduce graph
+            Z = find_critical_set(students=Ss)          #Crtical set   
 
+            for project in Z:
+                lecturer = project_lecturer[project]
+                L_u_k = to_list(Ljk[lecturer][project]) 
+                for student in L_u_k:
+                    if project in graph[student]:
+                        graph[student].remove(project)
+                        Gp[project].remove(student)
+                        Gl[lecturer].remove(student)
+                        lecturerC[lecturer] -= 1
+                        projectC[project] -= 1
 
+            if not Z:                                   #Break form seconf infinitive loop
+                break   
 
-def dominated(alpha, assign_students, lecturer_capacity, students):
-    dG = len(assign_students)
-    if min(alpah, dG) >= lecturer_capacity:
-        for pref_stu in students:
-
-
-
-
-
-                                
-
-
-
-def initialization(length):
-    init = {}
-    for i in range(length):
-        init[i] = []
-    return init
-
-def lp_initialization(lengthL, offers):
-    init = {}
-    for i in range(length):
-        init[i] = {}
-        projects = offers[i]
-        for pro in projects:
-            init[i][pro] = []
-    return init
-
-def lecturer_offers(offers):
-    project_offered = {}
-    i = 0
-
-    for offer in offers:
-        for p in offer:
-            project_offered[p] = i
-
-        i += 1
+            for p in Pp:
+                undersubscribed = True if len(Gp[P]) < projects_capacity[p] else False
+                if p in project_replete and undersubscribed:
+                    lecturer = project_lecturer[p]
+                    sr = None # Sr ???
+                    lec_pre = lecturers_pref[lecturer]
+                    last_pre_index = len(lec_pre) - 1
+                    Sr_index = lec_pre.index(sr)
+                    if Sr_index < last_pre_index:
+                        last_stu = to_list(lec_pre.pop())
+                        for student in lec_pre:
+                            intersection_projects = intersection(A=students_pref[student], B=offers[lecturer])
+                            for p in intersection_projects:
+                                if p in graph[student]:
+                                    graph[student].remove(p)
+                                    Gp[p].remove(student)
+                                    Gl[lecturer].remove(student)
+                                    lecturerC[lecturer] -= 1
+                                    projectC[p] -= 1
+    student = 0
+    for student_list in graph:
+        for project in student_list:        
+            alpha = calculate_alpha(projects_capacity=projects_capacity, Gp=Gp, projects=student_list)
+            is_lower_edge = lower_edge( student=student, project=project,
+                                        lecturer=lecturer_proposed[project], Lk=lecturers_pref, 
+                                        Gl=Gl, alpha=alpha, lecturers_capacity=lecturers_capacity)
+            if is_lower_edge:
+                lecturer = lecturer_proposed[project]
+                # unbounded edge 
+                # for on each unbounded edge
+    M = feasible_mathching()
+    if strong_matching(M):
+        return M
+    else:
+        print("no strongly stable matching exist")
     
-    return project_offered
--- source/methods.py
+++ source/methods.py
@@ -11,13 +11,91 @@ def generate_Ljk(students_pref, lecturers_pref, offers, number_of_lect):
                 if project in students_pref[student]:
                     L_jk[lecturer][project].append(student)
             
-
     return L_jk
 
-def calculate_alpha(project_capacity, Gp, projects):
+def initialization_list(length):
+    init = {}
+    for i in range(length):
+        init[i] = []
+    return init
+
+def calculate_alpha(projects_capacity, Gp, projects):
+    '''
+        alpha = SIGMA(qj of each project)
+        qj = min(capacity of pj, number of student provisionally assigned to pj)
+    '''
     alpha = 0
     for project in projects:
-        c = project_capacity[project]
-        dG = len(Gp[project])
-        alpha += min(c, dg)
+        c = projects_capacity[project]              #Capacity of project
+        dG = len(Gp[project])                       #Number of student provisionally assigned to project
+        alpha += min(c, dg)                         #SIGMA(qj of each project)  
     return alpha
+
+def dominated_Lk(alpha, lecturer_capacity, lecturer_pref, Gl, lecturer):
+    dG = len(Gl)
+    students = []
+    if min(alpah, dG) >= lecturer_capacity:
+        last_stu = to_list(lecturer_pref.pop())      
+        for ls in last_stu:
+            if ls in Gl[lecturer]:
+                students.append(ls)     
+    return students
+
+def dominated_Lkj(Ljk, lecturer, project, Gp):
+    last_stu = to_list(Ljk[lecturer][project].pop())
+    students = []
+    for ls in last_stu:
+        if ls in Gp[project]:
+            students.append(ls)
+    return students
+        
+def lecturer_proposed(offers):
+    '''
+        For each project, its lecturer is assigned
+    '''
+    project_offered = {}
+    i = 0
+    for offer in offers:
+        for p in offer:
+            project_offered[p] = i
+
+        i += 1
+    
+    return project_offered
+
+def to_list(date):
+    if type(date) == tuple:
+        return list(data)
+    else:
+        dataL = []
+        dataL.append(date)
+        return dataL
+    
+def intersection(A, B):
+    intersection = []
+    for a in A:
+        for b in B:
+            intersection.append(b) if a == b else None
+
+def reduce_graph(graph):
+    pass
+
+def bounded_edge(student, project, lecturer, Lk, Ljk, Gp, Gl, projects_capacity, is_lower_edge):
+    oversubscribed = True if len(Gp[project]) > project_capacity[project] else False
+    in_tail_Ljk = True if student == Ljk[lectrur][project].pop() else False
+    in_tail_Lk = True if student == Lk[lecturer].pop() else False
+
+    if not oversubscribed and not in_tail_Ljk:
+        if not is_lower_edge and not in_tail_Lk:
+            return True
+    return False
+
+def lower_edge(student, project, lecturer, Lk, Gl, alpha, lecturers_capacity):
+    in_tail_Lk = True if student == Lk[lecturer].pop() else False
+    check = True if min(len(Gl[lecturer]), alpha) > lecturers_capacity[lecturer] else False
+
+    return True if in_tail_Lk and check else False
+
+def find_critical_set(students):
+    pass
+
